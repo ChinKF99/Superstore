72d489567397a439ab1b2fcb3ba8f613dba473a5
ChinKF99 <136291127+ChinKF99@users.noreply.github.com>
2026-03-02 13:23:34 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import pandas as pd
+
+df = pd.read_csv('superstore.csv', encoding='latin1')
+
+# Replace "," with empty string
+df.product_name = df.product_name.str.replace(',', '', regex=False)
+
+output_filename = 'modified_superstore.csv'
+df.to_csv(output_filename, index=False)
+
+
+
+
